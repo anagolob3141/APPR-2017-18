27a8b53f92ae1756003b04d1c6cbeb7a096a1dd0
Ana Golob <ana.golob@student.fmf.uni-lj.si>
2018-01-18 10:00:16 +0100

    Popravek poti

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -30,20 +30,20 @@ imena.valut <- tabela.za.kratice[, 1:2] %>% unique()
 
 # Ker je uvoženih podatkov veliko jih bomo shranili v 3 csv datotek.
 # V vsaki se nahajajo podatki o valutah za eno tretjino leta 2016.
-if (!file.exists("podatki\\tecaji1tretjina.csv")){
+if (!file.exists("podatki/tecaji1tretjina.csv")){
   datumi1.tretjina2016 <- seq(as.Date("2016-01-01"), as.Date("2016-04-30"), by="days")
   tabela1.tretjina2016 <- uvoziPodatke(datumi1.tretjina2016)
   uredi.tabelo(tabela1.tretjina2016, "podatki\\tecaji1tretjina.csv")
 }
 
-if (!file.exists("podatki\\tecaji2tretjina.csv")){
+if (!file.exists("podatki/tecaji2tretjina.csv")){
   datumi2.tretjina2016 <- seq(as.Date("2016-05-01"), as.Date("2016-08-31"), by="days")
   tabela2.tretjina2016 <- uvoziPodatke(datumi2.tretjina2016)
-  uredi.tabelo(tabela2.tretjina2016, "podatki\\tecaji2tretjina.csv")
+  uredi.tabelo(tabela2.tretjina2016, "podatki/tecaji2tretjina.csv")
 }
 
 if (!file.exists("podatki\\tecaji3tretjina.csv")){
   datumi3.tretjina2016 <- seq(as.Date("2016-09-01"), as.Date("2016-12-31"), by="days")
   tabela3.tretjina2016 <- uvoziPodatke(datumi3.tretjina2016)
-  uredi.tabelo(tabela3.tretjina2016, "podatki\\tecaji3tretjina.csv")
+  uredi.tabelo(tabela3.tretjina2016, "podatki/tecaji3tretjina.csv")
 }
