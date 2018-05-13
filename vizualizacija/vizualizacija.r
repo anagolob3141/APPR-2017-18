d137741350a54aa77d63fe20f1e59f9f2614cc09
Ana Golob <ana.golob@student.fmf.uni-lj.si>
2018-05-13 20:05:19 +0200

    popravljanje zemljevida

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -9,9 +9,20 @@ pogostost <- as.data.frame(count(valute.po.drzavah, vars = valuta))
 pogostost <- merge(x=pogostost, y=imena.valut, by.x="vars", by.y="variable")
 pogostost <- pogostost[order(pogostost$n,decreasing = TRUE),]
 pogostost <- pogostost[, c(1,3,2)]
-colnames(pogostost) <- c("kratica valute","ime valute", "št.držav")
 row.names(pogostost) <- NULL
-pogoste.valute <- pogostost[pogostost$št.držav > 1,]
+
+pogoste.valute <- pogostost[pogostost$n > 1,]
+
+pogoste.valute$vars <- factor(pogoste.valute$vars,
+                                     levels = unique(pogoste.valute$vars)[order(pogoste.valute$n, decreasing = TRUE)])
+
+graf1 <- plot_ly() %>%
+  add_trace(data=pogoste.valute, x= ~vars, y=~n, type = 'bar') %>%
+  layout(title = 'Valute, ki se uporabljajo v več kot 1 državi', 
+       xaxis = list(title = "Valute", tickangle = 45),
+       yaxis = list (title = "Šrevilo držav v katerih se uporabljajo")
+  )
+graf1 
 
 
 # Države,z večjim številom državnih valut (prikazane na zemljevidu spodaj):
@@ -42,34 +53,38 @@ valute.centri <- left_join(centri,valute.drzav.s.skupinami, by="drzava")
 valute.po.svetu <- left_join(world,valute.drzav.s.skupinami, by="drzava")
 
 valute.po.svetu$valuta[valute.po.svetu$drzava == "United States of America"] <- "USD"
-valute.po.svetu$skupina[valute.po.svetu$drzava == "United States of America"] <- "v razvoju"
+valute.po.svetu$skupina[valute.po.svetu$drzava == "United States of America"] <- "najvecje"
 valute.centri$valuta[valute.centri$drzava == "United States of America"] <- "USD"
-valute.centri$skupina[valute.centri$drzava == "United States of America"] <- "v razvoju"
+valute.centri$skupina[valute.centri$drzava == "United States of America"] <- "najvecje"
+valute.centri$valuta[valute.centri$drzava == "Greenland"] <- "DKK"
+valute.po.svetu$valuta[valute.po.svetu$drzava == "Greenland"] <- "DKK"
 
-# GRAF SE NAHAJA V SHINY APLIKACIJI
-# valute.svet <- ggplot() + geom_polygon(data = valute.po.svetu,
-#                                        aes(x=long, y = lat, group = group,fill=valuta)) + coord_fixed(1.3) +
-#   theme(legend.position="bottom",legend.text = element_text(size=3), legend.key.size =  unit(0.09, "in")) +
-#   geom_text(data= valute.centri,
-#             aes(x = x, y = y, label = valuta),
-#             alpha = 1,
-#             color = "black",
-#             size = 1.5)
-# 
-# valute.svet
-#####################################################################
-# Zemljevid, ki prikazuje države z večjim številom državnih valut (nadajevanje od zgoraj):
-vec.valut.centri <- left_join(centri,vec.valut, by="drzava")
-vec.valut.drzave <- left_join(world,vec.valut, by="drzava")
+izloci <- c("Antigua and Barbuda", "Anguilla","Aruba", "Dominica", "Haiti", "Jamaica",
+            "Liechtenstein", "Sint Maarten", "Turks and Caicos Islands", "Grenada",
+            "Cayman Islands", "Northern Cyprus", "Saint Kitts and Nevis", "Saint Lucia", "Saint Martin",
+            "Montserrat","Puerto Rico", "Trinidad and Tobago", "British Virgin Islands", 
+            "United States Virgin Islands", "Saint Barthelemy", "Curacao", "Saint Vincent and the Grenadines",
+            "Barbados", "El Salvador","Belize","Honduras", "Bosnia and Herzegovina", "Guernsey", "Hungary",
+            "Moldova","Romania","French Polynesia","Wallis and Futuna","Samoa", "Tonga", "Niue", "American Samoa",
+            "Cook Islands", "Pitcairn Islands", "Sierra Leone", "Benin","Togo","Equatorial Guinea", 
+            "Republic of the Congo","Rwanda","Lesotho","Israel", "Lebanon","Qatar","Marshall Islands","Thailand",
+            "Laos","Armenia","Vanuatu") 
 
+izpisi.centri <- subset(valute.centri,!(valuta %in% "EUR"))
+izpisi.centri <- subset(izpisi.centri,!(drzava %in% izloci))
+izpisi.centri <- rbind(izpisi.centri, subset(valute.centri,drzava %in% c("Netherlands","Spain")))
 
-vec.valut.svet <- ggplot() + geom_polygon(data = vec.valut.drzave,
-                                          aes(x=long, y = lat, group = group,fill=št.valut)) + coord_fixed(1.3) +
-  geom_text(data= vec.valut.centri[vec.valut.centri$št.valut > 1,],
-            aes(x = x, y = y, label = drzava),
+valute.svet <- ggplot() + geom_polygon(data = valute.po.svetu,
+                                       aes(x=long, y = lat, group = group,fill=valuta)) + coord_fixed(1.3) +
+  theme(legend.position="none") +
+  ggtitle("Nacionalne valute držav") +
+  geom_text(data= izpisi.centri,
+            aes(x = x, y = y, label = valuta),
             alpha = 1,
-            color = "red",
-            size = 2.5)
+            color = "black",
+            size = 1.5)
+valute.svet
 
-vec.valut.svet
+#####################################################################
+
 
