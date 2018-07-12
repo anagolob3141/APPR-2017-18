449490604dfad9702f188ad962d96ce8b7c80009
Ana Golob <ana.golob@student.fmf.uni-lj.si>
2018-07-12 11:08:57 +0200

    tabsetPanel

--- shiny/ui.R
+++ shiny/ui.R
@@ -3,27 +3,38 @@ library(shiny)
 shinyUI(fluidPage(
   
   titlePanel("Valutni tečaji"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      
-      sliderInput(inputId = "ymax",
-                  label = "Razpon višin tečajev narisanih na grafu:",
-                  min = 0,
-                  max = 1800,
-                  value = c(0, 1800)),
-      
-      selectInput("primer", "Izberite skupino prikazanih valut:", 
-                  choices = c("Valute vseh držav", "Valute v katerih se največ trguje", "Valute držav v razvoju"))
-      
-    ),
+  tabsetPanel(
+      tabPanel("Časovne vrste tečajev", fluid = TRUE,
+               sidebarLayout(
+                 sidebarPanel(
+                   sliderInput(inputId = "ymax",
+                               label = "Razpon višin tečajev narisanih na grafu:",
+                               min = 0,
+                               max = 1800,
+                               value = c(0, 1800))
+                 ),
+                 mainPanel(
+                   plotlyOutput(outputId = "gra1"),
+                   textOutput("opis1")
+                 )
+      )),
     
-    mainPanel(
-      
-      tabsetPanel(type = "tabs",
-                  tabPanel("Gibanje valutnih tečajev",plotlyOutput(outputId = "gra1"), textOutput("opis1")),
-                  tabPanel("Nihanje in povprečje",plotlyOutput(outputId = "gra2"), textOutput("opis2"))
-      )
+      tabPanel("Varianca in povprečna višina odklonov", fluid = TRUE,
+               sidebarLayout(
+                 sidebarPanel(
+                   selectInput("primer", "Izberite skupino prikazanih valut:", 
+                               choices = c("Valute vseh držav", "Valute v katerih se največ trguje", "Valute držav v razvoju")),
+                   sliderInput(inputId = "ymax2",
+                               label = "Razpon višin tečajev narisanih na grafu:",
+                               min = 0,
+                               max = 1800,
+                               value = c(0, 1800))
+                 ),
+                 mainPanel(
+                   plotlyOutput(outputId = "gra2"), 
+                   textOutput("opis2")
+                 )
+    )
     )
   )
 ))
